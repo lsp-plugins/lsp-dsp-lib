5bef3192f14b2938ec4c6e7152f1e559a5a1caa9
sadko4u <sadko4u@gmail.com>
2020-05-14 23:07:00 +0300

    Updated dependencies

--- dependencies.mk
+++ dependencies.mk
@@ -10,12 +10,15 @@ LSP_TEST_FW_TYPE           := src
 LSP_TEST_FW_URL            := https://github.com/sadko4u/$(LSP_TEST_FW_NAME).git
 
 STDLIB_VERSION             := system
+STDLIB_TYPE                := opt
 STDLIB_LDFLAGS             := -lpthread
 
 ifeq ($(PLATFORM),Windows)
   TEST_STDLIB_VERSION        := system
+  TEST_STDLIB_TYPE           := opt
   TEST_STDLIB_LDFLAGS        := -lshlwapi
 else
   TEST_STDLIB_VERSION        := system
+  TEST_STDLIB_TYPE           := opt
   TEST_STDLIB_LDFLAGS        := 
 endif
