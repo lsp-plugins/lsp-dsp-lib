881007bbcf30680cc1fecc5fbc84769ca56897e8
sadko4u <sadko4u@gmail.com>
2020-07-20 15:07:10 +0300

    Version up

--- project.mk
+++ project.mk
@@ -2,7 +2,7 @@
 ARTIFACT_NAME               = lsp-dsp-lib
 ARTIFACT_VARS               = LSP_DSP_LIB
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 0.5.3
+ARTIFACT_VERSION            = 0.5.4-devel
 
 # List of dependencies
 TEST_DEPENDENCIES = \
