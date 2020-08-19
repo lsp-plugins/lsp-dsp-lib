90931a48ecc70ff0984a39de964d94764912395c
Alexandros Theodotou <alex@zrythm.org>
2020-08-19 18:14:04 +0100

    add C api function: lsp_dsp_lib_dsp_init

---
+++ include/lsp-plug.in/dsp/dsp_c.h
@@ -0,0 +1,24 @@
+/*
+ * dsp_c.h
+ *
+ *  Created on: 19 Aug 2020.
+ *      Author: Alexandros Theodotou
+ */
+
+#ifndef LSP_PLUG_IN_DSP_DSP_C_H_
+#define LSP_PLUG_IN_DSP_DSP_C_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <lsp-plug.in/dsp/version.h>
+
+void
+lsp_dsp_lib_dsp_init (void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LSP_PLUG_IN_DSP_DSP_C_H_ */
