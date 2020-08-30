6abc93e781934282bfd38a4f5a91ab0a37a69468
sadko4u <sadko4u@gmail.com>
2020-08-30 23:29:49 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -23,7 +23,7 @@ ARTIFACT_DESC               = DSP library for digital signal processing
 ARTIFACT_NAME               = lsp-dsp-lib
 ARTIFACT_VARS               = LSP_DSP_LIB
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 0.5.7-devel
+ARTIFACT_VERSION            = 0.5.7
 
 # List of dependencies
 DEPENDENCIES = \
