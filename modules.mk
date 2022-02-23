e1e88f193de8328cf0f45a6bbd6887bdedc390e7
sadko4u <sadko4u@gmail.com>
2022-02-24 01:11:58 +0300

    Preparing for release

--- modules.mk
+++ modules.mk
@@ -19,13 +19,13 @@
 #
 
 # Variables that describe dependencies
-LSP_COMMON_LIB_VERSION     := 1.0.14
+LSP_COMMON_LIB_VERSION     := 1.0.15
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_TYPE        := src
 LSP_COMMON_LIB_URL_RO      := https://github.com/lsp-plugins/$(LSP_COMMON_LIB_NAME).git
 LSP_COMMON_LIB_URL_RW      := git@github.com:lsp-plugins/$(LSP_COMMON_LIB_NAME).git
 
-LSP_TEST_FW_VERSION        := 1.0.8
+LSP_TEST_FW_VERSION        := 1.0.9
 LSP_TEST_FW_NAME           := lsp-test-fw
 LSP_TEST_FW_TYPE           := src
 LSP_TEST_FW_URL_RO         := https://github.com/lsp-plugins/$(LSP_TEST_FW_NAME).git
