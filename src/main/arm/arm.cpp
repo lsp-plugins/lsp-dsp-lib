a00d6a57c996246b7dc6db4b6121f3b96d2765d0
sadko4u <sadko4u@gmail.com>
2023-03-08 23:03:22 +0300

    Updated ARM conditional compilation

--- src/main/arm/arm.cpp
+++ src/main/arm/arm.cpp
@@ -21,7 +21,7 @@
 
 #include <lsp-plug.in/common/types.h>
 
-#ifdef ARCH_ARM
+#if defined(ARCH_ARM6) || defined(ARCH_ARM7)
     #include <private/dsp/exports.h>
     #include <lsp-plug.in/dsp/dsp.h>
     #include <lsp-plug.in/stdlib/math.h>
@@ -359,6 +359,6 @@
         }
     }
 
-#endif /* ARCH_ARM */
+#endif /* defined(ARCH_ARM6) || defined(ARCH_ARM7) */
 
 
