687a9c1c04038dd1a70e2c7901e1e6e2a1524981
sadko4u <sadko4u@gmail.com>
2023-10-02 19:25:15 +0300

    Refactoring: split unit test into two unit tests

--- src/test/utest/pcomplex/div2.cpp
+++ src/test/utest/pcomplex/div2.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2023 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2023 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-dsp-lib
  * Created on: 31 мар. 2020 г.
@@ -29,7 +29,6 @@ namespace lsp
     {
         void pcomplex_div2(float *dst, const float *src, size_t count);
         void pcomplex_rdiv2(float *dst, const float *src, size_t count);
-        void pcomplex_div3(float *dst, const float *t, const float *b, size_t count);
     }
 
     IF_ARCH_X86(
@@ -37,18 +36,15 @@ namespace lsp
         {
             void pcomplex_div2(float *dst, const float *src, size_t count);
             void pcomplex_rdiv2(float *dst, const float *src, size_t count);
-            void pcomplex_div3(float *dst, const float *t, const float *b, size_t count);
         }
 
         namespace avx
         {
             void pcomplex_div2(float *dst, const float *src, size_t count);
             void pcomplex_rdiv2(float *dst, const float *src, size_t count);
-            void pcomplex_div3(float *dst, const float *t, const float *b, size_t count);
 
             void pcomplex_div2_fma3(float *dst, const float *src, size_t count);
             void pcomplex_rdiv2_fma3(float *dst, const float *src, size_t count);
-            void pcomplex_div3_fma3(float *dst, const float *t, const float *b, size_t count);
         }
     )
 
@@ -57,7 +53,6 @@ namespace lsp
         {
             void pcomplex_div2(float *dst, const float *src, size_t count);
             void pcomplex_rdiv2(float *dst, const float *src, size_t count);
-            void pcomplex_div3(float *dst, const float *t, const float *b, size_t count);
         }
     )
 
@@ -66,15 +61,13 @@ namespace lsp
         {
             void pcomplex_div2(float *dst, const float *src, size_t count);
             void pcomplex_rdiv2(float *dst, const float *src, size_t count);
-            void pcomplex_div3(float *dst, const float *t, const float *b, size_t count);
         }
     )
 
     typedef void (* pcomplex_div2_t) (float *dst, const float *src, size_t count);
-    typedef void (* pcomplex_div3_t) (float *dst, const float *t, const float *b, size_t count);
 }
 
-UTEST_BEGIN("dsp.pcomplex", div)
+UTEST_BEGIN("dsp.pcomplex", div2)
 
     void call(const char *text,  size_t align, pcomplex_div2_t func1, pcomplex_div2_t func2)
     {
@@ -114,47 +107,6 @@ UTEST_BEGIN("dsp.pcomplex", div)
         }
     }
 
-    void call(const char *text,  size_t align, pcomplex_div3_t func1, pcomplex_div3_t func2)
-    {
-        if (!UTEST_SUPPORTED(func1))
-            return;
-        if (!UTEST_SUPPORTED(func2))
-            return;
-
-        UTEST_FOREACH(count, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-                32, 33, 37, 48, 49, 64, 65, 0x3f, 100, 999, 0x1fff)
-        {
-            for (size_t mask=0; mask <= 0x07; ++mask)
-            {
-                printf("Testing %s on input buffer of %d numbers, mask=0x%x...\n", text, int(count), int(mask));
-
-                FloatBuffer src1(count*2, align, mask & 0x01);
-                FloatBuffer src2(count*2, align, mask & 0x02);
-                FloatBuffer dst1(count*2, align, mask & 0x04);
-                FloatBuffer dst2(count*2, align, mask & 0x04);
-
-                // Call functions
-                func1(dst1, src1, src2, count);
-                func2(dst2, src1, src2, count);
-
-                UTEST_ASSERT_MSG(src1.valid(), "Source buffer 1corrupted");
-                UTEST_ASSERT_MSG(src2.valid(), "Source buffer 2 corrupted");
-                UTEST_ASSERT_MSG(dst1.valid(), "Destination buffer 1 corrupted");
-                UTEST_ASSERT_MSG(dst2.valid(), "Destination buffer 2 corrupted");
-
-                // Compare buffers
-                if (!dst2.equals_absolute(dst1, 1e-4))
-                {
-                    src1.dump("src1");
-                    src2.dump("src2");
-                    dst1.dump("dst1");
-                    dst2.dump("dst2");
-                    UTEST_FAIL_MSG("Output of functions for test '%s' differs", text);
-                }
-            }
-        }
-    }
-
     UTEST_MAIN
     {
         #define CALL(generic, func, align) \
@@ -162,22 +114,16 @@ UTEST_BEGIN("dsp.pcomplex", div)
 
         IF_ARCH_X86(CALL(generic::pcomplex_div2, sse::pcomplex_div2, 16));
         IF_ARCH_X86(CALL(generic::pcomplex_rdiv2, sse::pcomplex_rdiv2, 16));
-        IF_ARCH_X86(CALL(generic::pcomplex_div3, sse::pcomplex_div3, 16));
-
         IF_ARCH_X86(CALL(generic::pcomplex_div2, avx::pcomplex_div2, 32));
         IF_ARCH_X86(CALL(generic::pcomplex_div2, avx::pcomplex_div2_fma3, 32));
         IF_ARCH_X86(CALL(generic::pcomplex_rdiv2, avx::pcomplex_rdiv2, 32));
         IF_ARCH_X86(CALL(generic::pcomplex_rdiv2, avx::pcomplex_rdiv2_fma3, 32));
-        IF_ARCH_X86(CALL(generic::pcomplex_div3, avx::pcomplex_div3, 32));
-        IF_ARCH_X86(CALL(generic::pcomplex_div3, avx::pcomplex_div3_fma3, 32));
 
         IF_ARCH_ARM(CALL(generic::pcomplex_div2, neon_d32::pcomplex_div2, 16));
         IF_ARCH_ARM(CALL(generic::pcomplex_rdiv2, neon_d32::pcomplex_rdiv2, 16));
-        IF_ARCH_ARM(CALL(generic::pcomplex_div3, neon_d32::pcomplex_div3, 16));
 
         IF_ARCH_AARCH64(CALL(generic::pcomplex_div2, asimd::pcomplex_div2, 16));
         IF_ARCH_AARCH64(CALL(generic::pcomplex_rdiv2, asimd::pcomplex_rdiv2, 16));
-        IF_ARCH_AARCH64(CALL(generic::pcomplex_div3, asimd::pcomplex_div3, 16));
     }
 
 UTEST_END;
