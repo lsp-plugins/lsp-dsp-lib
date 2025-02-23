3b15f3e74979139000b95066de37c649f0ab4b56
sadko4u <sadko4u@gmail.com>
2025-02-23 21:02:23 +0300

    Implemented reverse_fft routine optimized for AVX-512

--- include/private/dsp/arch/x86/avx512/fft.h
+++ include/private/dsp/arch/x86/avx512/fft.h
@@ -94,25 +94,25 @@ namespace lsp
         {
             if (rank == 2)
             {
-                float s0_re     = src_re[0] + src_re[1];
-                float s1_re     = src_re[0] - src_re[1];
-                float s2_re     = src_re[2] + src_re[3];
-                float s3_re     = src_re[2] - src_re[3];
+                float s0_re     = src_re[0] + src_re[2];
+                float s1_re     = src_re[0] - src_re[2];
+                float s2_re     = src_re[1] + src_re[3];
+                float s3_re     = src_re[1] - src_re[3];
 
-                float s0_im     = src_im[0] + src_im[1];
-                float s1_im     = src_im[0] - src_im[1];
-                float s2_im     = src_im[2] + src_im[3];
-                float s3_im     = src_im[2] - src_im[3];
+                float s0_im     = src_im[0] + src_im[2];
+                float s1_im     = src_im[0] - src_im[2];
+                float s2_im     = src_im[1] + src_im[3];
+                float s3_im     = src_im[1] - src_im[3];
 
-                dst_re[0]       = s0_re + s2_re;
-                dst_re[1]       = s1_re + s3_im;
-                dst_re[2]       = s0_re - s2_re;
-                dst_re[3]       = s1_re - s3_im;
+                dst_re[0]       = (s0_re + s2_re) * 0.25f;
+                dst_re[1]       = (s1_re - s3_im) * 0.25f;
+                dst_re[2]       = (s0_re - s2_re) * 0.25f;
+                dst_re[3]       = (s1_re + s3_im) * 0.25f;
 
-                dst_im[0]       = s0_im + s2_im;
-                dst_im[1]       = s1_im - s3_re;
-                dst_im[2]       = s0_im - s2_im;
-                dst_im[3]       = s1_im + s3_re;
+                dst_im[0]       = (s0_im + s2_im) * 0.25f;
+                dst_im[1]       = (s1_im + s3_re) * 0.25f;
+                dst_im[2]       = (s0_im - s2_im) * 0.25f;
+                dst_im[3]       = (s1_im - s3_re) * 0.25f;
             }
             else if (rank == 1)
             {
@@ -120,10 +120,10 @@ namespace lsp
                 // s1' = s0 - s1
                 float s1_re     = src_re[1];
                 float s1_im     = src_im[1];
-                dst_re[1]       = src_re[0] - s1_re;
-                dst_im[1]       = src_im[0] - s1_im;
-                dst_re[0]       = src_re[0] + s1_re;
-                dst_im[0]       = src_im[0] + s1_im;
+                dst_re[1]       = (src_re[0] - s1_re) * 0.5f;
+                dst_im[1]       = (src_im[0] - s1_im) * 0.5f;
+                dst_re[0]       = (src_re[0] + s1_re) * 0.5f;
+                dst_im[0]       = (src_im[0] + s1_im) * 0.5f;
             }
             else
             {
@@ -189,7 +189,7 @@ namespace lsp
             }
 
             for (size_t i=4; i < rank; ++i)
-                butterfly_direct16p(dst_re, dst_im, i, 1 << (rank - i - 1));
+                butterfly_reverse16p(dst_re, dst_im, i, 1 << (rank - i - 1));
 
             dsp::normalize_fft2(dst_re, dst_im, rank);
         }
