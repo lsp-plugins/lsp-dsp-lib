aee717c228dae77df15329267daf3876db3366d7
sadko4u <sadko4u@gmail.com>
2025-11-27 11:19:27 +0300

    SSE optimization of clamp functions

--- src/main/x86/sse.cpp
+++ src/main/x86/sse.cpp
@@ -292,6 +292,11 @@
                 EXPORT1(reverse1);
                 EXPORT1(reverse2);
 
+                EXPORT1(clamp_vv1);
+                EXPORT1(clamp_vv2);
+                EXPORT1(clamp_kk1);
+                EXPORT1(clamp_kk2);
+
                 EXPORT1(direct_fft);
                 EXPORT1(reverse_fft);
                 EXPORT1(normalize_fft2);
