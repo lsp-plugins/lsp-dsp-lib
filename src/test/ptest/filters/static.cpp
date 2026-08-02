acf562379c01952397a0748d31e96bcdf1525b4d
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-08-02 03:54:04 +0300

    Fixed compilation for i686

--- src/test/ptest/filters/static.cpp
+++ src/test/ptest/filters/static.cpp
@@ -329,7 +329,7 @@ PTEST_BEGIN("dsp.filters", static, 5, 1000)
             IF_ARCH_X86(process_1x16("sse::biquad_process_x16 x1", out, in, sse::biquad_process_x16));
             IF_ARCH_X86_64(process_1x16("sse3::x64_biquad_process_x16 x1", out, in, sse3::x64_biquad_process_x16));
             IF_ARCH_X86(process_1x16("avx::biquad_process_x16_fma3 x1", out, in, avx::biquad_process_x16_fma3));
-            IF_ARCH_X86(process_1x16("avx::x64_biquad_process_x16_fma3 x1", out, in, avx::x64_biquad_process_x16_fma3));
+            IF_ARCH_X86_64(process_1x16("avx::x64_biquad_process_x16_fma3 x1", out, in, avx::x64_biquad_process_x16_fma3));
             IF_ARCH_X86(process_1x16("avx512::biquad_process_x16 x1", out, in, avx512::biquad_process_x16));
             IF_ARCH_ARM(process_1x16("neon_d32::biquad_process_x16 x1", out, in, neon_d32::biquad_process_x16));
             IF_ARCH_AARCH64(process_1x16("asimd::biquad_process_x16 x1", out, in, asimd::biquad_process_x16));
